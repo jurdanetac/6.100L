d5c96a728883ca5415b74d1339456c5cf6e256b6
Juan Urdaneta <jurdanetac10@gmail.com>
2026-06-30 12:40:58 -0400

    Add header to fe1.py

--- finger_exercises/fe1.py
+++ finger_exercises/fe1.py
@@ -1,3 +1,8 @@
+# Finger Exercise Lecture 1
+# Name: Juan Urdaneta
+# Collaborators: None
+# Time Spent: 0:05
+
 # Assume three variables are already defined for you: a, b, and c.
 # Create a variable called total that adds a and b then multiplies the result by c.
 # Include a last line in your code to print the value: print(total)
