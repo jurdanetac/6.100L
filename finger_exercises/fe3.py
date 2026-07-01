79b45609f0dafd0c7c041ec012fb3912a9a005a8
Juan Urdaneta <jurdanetac10@gmail.com>
2026-07-01 11:46:47 -0400

    Create fe3.py

---
+++ finger_exercises/fe3.py
@@ -0,0 +1,16 @@
+# Finger Exercise Lecture 3
+# Name: Juan Urdaneta
+# Collaborators: None
+# Time Spent: 0:05
+
+# Assume you are given a positive integer variable named N.
+# Write a piece of Python code that prints hello world on separate lines, N times.
+# You can use either a while loop or a for loop.
+
+for n in range(N):
+  print("hello world")
+
+i = 0
+while i < N:
+  print("hello world")
+  i +=1
